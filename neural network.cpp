678433c13bfdcef915450dc3bca84a2c9ad18bd9
NikitaMV <nikitagame1990@mail.ru>
2017-01-15 19:34:46 +0300

    Add files via upload

--- neural network.cpp
+++ neural network.cpp
@@ -1,7 +1,7 @@
-/* Задача: написать программу моделирования прогнозирующей нейронной сети.
-Обучение и прогнозирование производить на 30 и 15 значениях соответственно.
+п»ї/* Р—Р°РґР°С‡Р°: РЅР°РїРёСЃР°С‚СЊ РїСЂРѕРіСЂР°РјРјСѓ РјРѕРґРµР»РёСЂРѕРІР°РЅРёСЏ РїСЂРѕРіРЅРѕР·РёСЂСѓСЋС‰РµР№ РЅРµР№СЂРѕРЅРЅРѕР№ СЃРµС‚Рё.
+РћР±СѓС‡РµРЅРёРµ Рё РїСЂРѕРіРЅРѕР·РёСЂРѕРІР°РЅРёРµ РїСЂРѕРёР·РІРѕРґРёС‚СЊ РЅР° 30 Рё 15 Р·РЅР°С‡РµРЅРёСЏС… СЃРѕРѕС‚РІРµС‚СЃС‚РІРµРЅРЅРѕ.
 
-Автор: Мядель Никита */
+РђРІС‚РѕСЂ: РњСЏРґРµР»СЊ РќРёРєРёС‚Р° */
 
 
 #include <iostream>
@@ -10,9 +10,9 @@
 #include <ctime>
 using namespace std;
 
-double a = 0.0283;			//значение шага обучения 					
+double a = 0.0283;			//Р·РЅР°С‡РµРЅРёРµ С€Р°РіР° РѕР±СѓС‡РµРЅРёСЏ 					
 
-double test_function(double x)		//функция для моделирования
+double test_function(double x)		//С„СѓРЅРєС†РёСЏ РґР»СЏ РјРѕРґРµР»РёСЂРѕРІР°РЅРёСЏ
 {
 	double result;
 	double a=7;
@@ -24,25 +24,25 @@ double test_function(double x)		//функция для моделирования
 	return result;
 }
 
-double neural_function(double y1, double y2, double y3, double w1, double w2, double w3, double T)  //линейная функция активации нейронной сети
+double neural_function(double y1, double y2, double y3, double w1, double w2, double w3, double T)  //Р»РёРЅРµР№РЅР°СЏ С„СѓРЅРєС†РёСЏ Р°РєС‚РёРІР°С†РёРё РЅРµР№СЂРѕРЅРЅРѕР№ СЃРµС‚Рё
 {
 	double result = y1*w1 + y2*w2 + y3*w3 - T;
 	return result;
 }
 
-double calculate_error(double actual, double etalon)			//среднеквадратичная ошибка сети
+double calculate_error(double actual, double etalon)			//СЃСЂРµРґРЅРµРєРІР°РґСЂР°С‚РёС‡РЅР°СЏ РѕС€РёР±РєР° СЃРµС‚Рё
 {
 	double result = (actual - etalon)*(actual - etalon) / 2;
 	return result;
 }
 
-double change_W(double W0, double y, double actual, double etalon, double a)  //функция изменения весов нейронной сети
+double change_W(double W0, double y, double actual, double etalon, double a)  //С„СѓРЅРєС†РёСЏ РёР·РјРµРЅРµРЅРёСЏ РІРµСЃРѕРІ РЅРµР№СЂРѕРЅРЅРѕР№ СЃРµС‚Рё
 {
 	double result = W0 - a*(actual - etalon)*y;
 	return result;
 }
 
-double change_T(double T0, double actual, double etalon, double a)   //функция изменения порога нейронной сети
+double change_T(double T0, double actual, double etalon, double a)   //С„СѓРЅРєС†РёСЏ РёР·РјРµРЅРµРЅРёСЏ РїРѕСЂРѕРіР° РЅРµР№СЂРѕРЅРЅРѕР№ СЃРµС‚Рё
 {
 	double result = T0 + a*(actual - etalon);
 	return result;
@@ -55,7 +55,7 @@ int main()
 
 	ofstream fout("output.txt");	
 	
-	double Em = 0.001;		//Желаемая суммарная квадратичная ошибка сети
+	double Em = 0.001;		//Р–РµР»Р°РµРјР°СЏ СЃСѓРјРјР°СЂРЅР°СЏ РєРІР°РґСЂР°С‚РёС‡РЅР°СЏ РѕС€РёР±РєР° СЃРµС‚Рё
 
 	double x[45], y[45];
 
@@ -74,7 +74,7 @@ int main()
 
 		if (number_of_iterations > 1000)
 		{
-			cout << "Количество итераций превышает максимально допустимое. Принудительный выход из цикла" << endl<<endl;
+			cout << "РљРѕР»РёС‡РµСЃС‚РІРѕ РёС‚РµСЂР°С†РёР№ РїСЂРµРІС‹С€Р°РµС‚ РјР°РєСЃРёРјР°Р»СЊРЅРѕ РґРѕРїСѓСЃС‚РёРјРѕРµ. РџСЂРёРЅСѓРґРёС‚РµР»СЊРЅС‹Р№ РІС‹С…РѕРґ РёР· С†РёРєР»Р°" << endl<<endl;
 			break;
 		}
 
@@ -89,8 +89,8 @@ int main()
 
 		if ((E - Em) < 0.00000001)
 		{
-			cout << "Число итераций: " << number_of_iterations << endl;
-			fout << "Число итераций: " << number_of_iterations << endl;
+			cout << "Р§РёСЃР»Рѕ РёС‚РµСЂР°С†РёР№: " << number_of_iterations << endl;
+			fout << "Р§РёСЃР»Рѕ РёС‚РµСЂР°С†РёР№: " << number_of_iterations << endl;
 			break;
 		}
 
@@ -98,7 +98,7 @@ int main()
 		{
 			NeuralValue = neural_function(y[i], y[i + 1], y[i + 2], w1, w2, w3, T);					
 
-			w1 = change_W(w1, y[i], NeuralValue, y[i + 3], a);				//обучение нейронной сети - изменение коэффициентов
+			w1 = change_W(w1, y[i], NeuralValue, y[i + 3], a);				//РѕР±СѓС‡РµРЅРёРµ РЅРµР№СЂРѕРЅРЅРѕР№ СЃРµС‚Рё - РёР·РјРµРЅРµРЅРёРµ РєРѕСЌС„С„РёС†РёРµРЅС‚РѕРІ
 			w2 = change_W(w2, y[i + 1], NeuralValue, y[i + 3], a);
 			w3 = change_W(w3, y[i + 2], NeuralValue, y[i + 3], a);
 			T = change_T(T, NeuralValue, y[i + 3], a);
@@ -106,20 +106,20 @@ int main()
 
 	}	
 	
-	cout << "Квадратичная ошибка сети: " << E << endl;
-	fout << "Квадратичная ошибка сети: " << E << endl;
+	cout << "РљРІР°РґСЂР°С‚РёС‡РЅР°СЏ РѕС€РёР±РєР° СЃРµС‚Рё: " << E << endl;
+	fout << "РљРІР°РґСЂР°С‚РёС‡РЅР°СЏ РѕС€РёР±РєР° СЃРµС‚Рё: " << E << endl;
 
 	cout << "w1: " << w1 << "\tw2: " << w2 << "\tw3: " << w3 << "\tT: " << T << endl << endl;
 	fout << "w1: " << w1 << "\tw2: " << w2 << "\tw3: " << w3 << "\tT: " << T << endl << endl;
 
-	cout << "Прогнозирование:" << endl;
-	fout << "Прогнозирование:" << endl;
+	cout << "РџСЂРѕРіРЅРѕР·РёСЂРѕРІР°РЅРёРµ:" << endl;
+	fout << "РџСЂРѕРіРЅРѕР·РёСЂРѕРІР°РЅРёРµ:" << endl;
 
-	for (int i = 27; i <= 41; i++)			//Прогнозирование значений моделируемой функции с помощью обученной нейронной сети
+	for (int i = 27; i <= 41; i++)			//РџСЂРѕРіРЅРѕР·РёСЂРѕРІР°РЅРёРµ Р·РЅР°С‡РµРЅРёР№ РјРѕРґРµР»РёСЂСѓРµРјРѕР№ С„СѓРЅРєС†РёРё СЃ РїРѕРјРѕС‰СЊСЋ РѕР±СѓС‡РµРЅРЅРѕР№ РЅРµР№СЂРѕРЅРЅРѕР№ СЃРµС‚Рё
 	{
 		double result = neural_function(y[i], y[i + 1], y[i + 2], w1, w2, w3, T);
-		cout << "Значение сети: " << result << "\t" << "Эталон: " << y[i + 3] << "  \tОшибка: " << abs(result - y[i + 3]) << endl;
-		fout << "Значение сети: " << result << "\t" << "Эталон: " << y[i + 3] << "  \tОшибка: " << abs(result - y[i + 3]) << endl;		
+		cout << "Р—РЅР°С‡РµРЅРёРµ СЃРµС‚Рё: " << result << "\t" << "Р­С‚Р°Р»РѕРЅ: " << y[i + 3] << "  \tРћС€РёР±РєР°: " << abs(result - y[i + 3]) << endl;
+		fout << "Р—РЅР°С‡РµРЅРёРµ СЃРµС‚Рё: " << result << "\t" << "Р­С‚Р°Р»РѕРЅ: " << y[i + 3] << "  \tРћС€РёР±РєР°: " << abs(result - y[i + 3]) << endl;		
 	}
 
 	return 0;
